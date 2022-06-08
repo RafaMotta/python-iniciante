2e30fcd81adb5c17d5b19e93bf83489712b878ef
Rafael Motta <34509225+RafaMotta@users.noreply.github.com>
2022-06-08 13:26:10 -0300

    init

---
+++ iniciante.py
@@ -0,0 +1,47 @@
+valor= 4
+dias=30
+horas=8
+
+pagamento=valor*horas*dias
+float(valor)
+int(10.5)
+
+print(pagamento)
+
+nome="Pedro Silva"
+nome2="Marcos Santos"
+nome[0:3]
+
+
+lista=[1,2,88,65, "teste"]
+lista.append("python")
+print(lista)
+lista.append(45)
+print(lista)
+lista.index("teste")
+lista.count(65)
+lista.append(65)
+lista.count(65)
+lista.remove("teste")
+print(lista)
+lista.reverse()
+print(lista)
+lista2=[1,5,2,47,32,5,3,18]
+lista2.sort()
+print(lista2)
+
+idades={"joao": 15, "pedro": 25, "maicon": 45}
+idades
+idades["maria"] = 23
+idades
+del idades["pedro"]
+idades
+
+tuplas=("marcos", "python", "aula", 6)
+tuplas[0]
+tuplas[1]
+tuplas[2]
+len(tuplas)
+tuplas+tuplas
+tuplas*3
+6 in tuplas
