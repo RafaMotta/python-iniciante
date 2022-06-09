a45748af1211d7a73e8717aabe87d36384dffdbd
Rafael Motta <34509225+RafaMotta@users.noreply.github.com>
2022-06-09 16:01:29 -0300

    condicionais

---
+++ condicionais.py
@@ -0,0 +1,36 @@
+numero=1
+	if(numero==1):
+		print("numero é igual a ", numero)
+	elif(numero==2):
+		print("numero é igual a ", numero)
+	else: 
+		pass
+        
+numero=10
+nome="Marcos"
+for x in numero:
+    print(numero)
+for letra in nome:
+    print(letra)
+    
+numero = 10
+while (numero > 0)
+    print(numero)
+    numero=numero-1
+    
+numero= 12
+while True:
+    numero=numero-1
+    print(numero)
+    if(numero==2):
+        break
+  
+
+numero = 10
+while True:
+    numero=numero-1
+    if(numero==4):
+        continue
+    print(numero)
+    if(numero==2):
+        break
